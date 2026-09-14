264be01ecef56c32b5ccb3ee8533c2c58d446667
Akhilesh Ibrahimpet <akhileshibrahimpet@gmail.com>
2026-09-14 22:23:58 +0530

    Add Project 2 package

---
+++ 02_class_based_sv_dv/tb/register_pkg.sv
@@ -0,0 +1,10 @@
+package register_pkg;
+
+  `include "transaction.sv"
+  `include "generator.sv"
+  `include "driver.sv"
+  `include "monitor.sv"
+  `include "scoreboard.sv"
+  `include "environment.sv"
+
+endpackage
